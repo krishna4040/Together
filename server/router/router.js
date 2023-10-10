78640f08c057b4412d2b0e24a3304e56e035d17a
krishna jain <115697713+krishna4040@users.noreply.github.com>
2023-10-11 00:24:40 +0530

    sorry forgot to add auth middleware so added

--- server/router/router.js
+++ server/router/router.js
@@ -8,20 +8,20 @@ const { updateProfile, deleteUser, getUserDetails } = require('../controllers/pr
 const { createPost, deletePost, likePost, commentPost } = require('../controllers/post');
 const { makeFriend, removeFriend } = require('../controllers/friend');
 
-router.post('/signup',signup);
-router.post('/login',login);
-router.post('/sendotp',sendotp);
+router.post('/signup', signup);
+router.post('/login', login);
+router.post('/sendotp', sendotp);
 
-router.post('/updateProfile',updateProfile);
-router.delete('/deleteUser',deleteUser);
-router.get('/getUserDetails',getUserDetails);
+router.post('/updateProfile', auth, updateProfile);
+router.delete('/deleteUser', auth, deleteUser);
+router.get('/getUserDetails', auth, getUserDetails);
 
-router.post('/createPost',createPost);
-router.delete('/deletePost',deletePost);
-router.post('/likePost',likePost);
-router.post('/commentPost',commentPost);
+router.post('/createPost', auth, createPost);
+router.delete('/deletePost', auth, deletePost);
+router.post('/likePost', auth, likePost);
+router.post('/commentPost', auth, commentPost);
 
-router.post('/makeFriend',makeFriend);
-router.delete('/removeFriend',removeFriend);
+router.post('/makeFriend', auth, makeFriend);
+router.delete('/removeFriend', auth, removeFriend);
 
 module.exports = router;
