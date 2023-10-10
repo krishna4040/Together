8d3f2d531a0aebe07feca894731b3f255147efc7
krishna jain <115697713+krishna4040@users.noreply.github.com>
2023-10-11 00:08:29 +0530

    friends add and remove controllers added

---
+++ server/controllers/friend.js
@@ -0,0 +1,47 @@
+const User = require('../models/User');
+
+exports.makeFriend = async (req, res) => {
+    try {
+        const { friendId } = req.body;
+        const userId = req.user.id;
+        if (!friendId) {
+            throw new Error('friend id is requiered');
+        }
+        const user = await User.findByIdAndUpdate(userId, { $push: { friends: friendId } });
+        if (!user) {
+            throw new Error('unable to make friend');
+        }
+        res.status(200).json({
+            success: true,
+            message: 'friend added succesfully'
+        });
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            message: error
+        });
+    }
+}
+
+exports.removeFriend = async (req, res) => {
+    try {
+        const { friendId } = req.body;
+        const userId = req.user.id;
+        if (!friendId) {    
+            throw new Error('friend id is requiered');
+        }
+        const user = await User.findByIdAndUpdate(userId,{$pull:{friends: friendId}});
+        if (!user) {
+            throw new Error('unable to remove friend');
+        }
+        res.status(200).json({
+            success: true,
+            message: 'friend removed succesfully'
+        });
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            message: error
+        });
+    }
+}
--- server/models/User.js
+++ server/models/User.js
@@ -11,7 +11,7 @@ const userSchema = new mongoose.Schema({
     friends: [
         {
             type: mongoose.Schema.Types.ObjectId,
-            ref: 'Friend'
+            ref: 'User'
         }
     ],
     posts: [
