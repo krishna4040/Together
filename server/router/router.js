8bce30fbf0032e19353566f3dd32ec5203fbfe41
krishna jain <115697713+krishna4040@users.noreply.github.com>
2023-10-11 00:23:07 +0530

    added routes for all controllers

--- server/router/router.js
+++ server/router/router.js
@@ -1,8 +1,27 @@
 const express = require('express');
 const router = express.Router();
 
-// import second from '../controllers/'
+const { auth } = require('../middlewares/auth');
 
-// router.get('/getBlogs',getBlog);
+const { signup, sendotp, login } = require('../controllers/auth');
+const { updateProfile, deleteUser, getUserDetails } = require('../controllers/profile');
+const { createPost, deletePost, likePost, commentPost } = require('../controllers/post');
+const { makeFriend, removeFriend } = require('../controllers/friend');
+
+router.post('/signup',signup);
+router.post('/login',login);
+router.post('/sendotp',sendotp);
+
+router.post('/updateProfile',updateProfile);
+router.delete('/deleteUser',deleteUser);
+router.get('/getUserDetails',getUserDetails);
+
+router.post('/createPost',createPost);
+router.delete('/deletePost',deletePost);
+router.post('/likePost',likePost);
+router.post('/commentPost',commentPost);
+
+router.post('/makeFriend',makeFriend);
+router.delete('/removeFriend',removeFriend);
 
 module.exports = router;
