4ec9eb3cb002a3269835e6f57542fd0d1bbc905c
krishna jain <krishnajain5050@gmail.com>
2024-06-15 01:56:20 +0530

    notification schema added

---
+++ server/models/Notification.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+
+const notificationSchema = new mongoose.Schema({
+    sender: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    content: String,
+    notificationType: {
+        type: String,
+        enum: ['post','friend-request', 'chat'],
+        required: true,
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now(),
+        expires: 24 * 7 * 60 * 60 * 1000, // 7 days
+    }
+})
+
+const Notification = mongoose.model('Notification', notificationSchema);
+
+module.exports = Notification;
