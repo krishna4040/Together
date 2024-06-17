02d760686c59eb32a63d70618bfa4208b81e0f96
krishna jain <krishnajain5050@gmail.com>
2024-06-17 22:55:32 +0530

    auth page bg changed

--- src/pages/Auth.jsx
+++ src/pages/Auth.jsx
@@ -1,6 +1,5 @@
 import React, { useState } from 'react'
 import insta from '../assets/insta.png'
-import bg from '../assets/bg.svg'
 import LoginForm from '../components/core/Auth/LoginForm'
 import SignupForm from '../components/core/Auth/SignupForm'
 import VerificationForm from '../components/core/Auth/VeificationForm'
@@ -11,15 +10,15 @@ const Auth = () => {
     const [tab, setTab] = useState('signup');
 
     return (
-        <section className='flex flex-col justify-center h-screen gap-10 p-2 bg-cover lg:gap-5 lg:overflow-hidden' style={{
-            backgroundImage: `url(${bg})`,
-        }}>
-            <h1 className='p-2 text-5xl font-bold text-center text-white font-Confortaa'>Together</h1>
+        <div className="relative h-screen w-screen bg-black overflow-y-hidden flex flex-col">
+            <div className="absolute bottom-0 left-0 right-0 top-0 bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#8080800a_1px,transparent_1px)] bg-[size:14px_24px]"></div>
+            <div className="absolute left-0 right-0 top-[-10%] h-[1000px] w-[1000px] rounded-full bg-[radial-gradient(circle_400px_at_50%_300px,#fbfbfb36,#000)]"></div>
+            <h1 className='p-2 text-5xl font-bold text-center text-white font-Confortaa z-10 mt-7'>Together</h1>
             <div className='flex flex-col items-center justify-center gap-20 p-2 lg:flex-row'>
                 <div className='items-center justify-center hidden overflow-hidden lg:flex'>
-                    <img src={insta} alt="image" className='' />
+                    <img src={insta} alt="image" />
                 </div>
-                <div className='flex flex-col w-full gap-5 lg:w-1/2'>
+                <div className='flex flex-col w-full gap-5 lg:w-1/2 z-10'>
                     <div className='flex items-center justify-center gap-10'>
                         <button onClick={() => { setTab('signup') }} className={`text-white uppercase transition-all duration-200 ${tab === 'signup' || tab === 'verification' || tab === 'profile' ? 'btn solid info' : ''}`}>
                             {(tab === 'signup' || tab === 'login') && "Signup"}
@@ -36,7 +35,7 @@ const Auth = () => {
                     </div>
                 </div>
             </div>
-        </section>
+        </div>
     )
 }
 
